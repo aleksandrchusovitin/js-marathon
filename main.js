c6909a172d6c65dc4efea49d8077e50da4bb1341
Aleksandr Chusovitin <chusovitinalexandr@gmail.com>
2020-10-01 23:50:33 +0700

    Home Work5

--- main.js
+++ main.js
@@ -1,30 +1,34 @@
 "use strict";
 
-const btn = document.getElementById("btn-kick");
-const btn2 = document.getElementById("btn-kick2");
+function getElById(id) {
+  return document.getElementById(id);
+}
+
+const btn = getElById("btn-kick");
+const btn2 = getElById("btn-kick2");
 
 const character = {
   name: "Pikachu",
   defaultHp: 200,
   damage: 200,
-  elHp: document.getElementById("health-character"),
-  elProgressbar: document.getElementById("progressbar-character"),
-  renderHP: renderHP,
-  renderHPLife: renderHPLife,
-  renderProgressbarHP: renderProgressbarHP,
-  changeHP: changeHP,
+  elHp: getElById("health-character"),
+  elProgressbar: getElById("progressbar-character"),
+  renderHP,
+  renderHPLife,
+  renderProgressbarHP,
+  changeHP,
 };
 
 const enemy = {
   name: "Charmander",
   defaultHp: 350,
   damage: 350,
-  elHp: document.getElementById("health-enemy"),
-  elProgressbar: document.getElementById("progressbar-enemy"),
-  renderHP: renderHP,
-  renderHPLife: renderHPLife,
-  renderProgressbarHP: renderProgressbarHP,
-  changeHP: changeHP,
+  elHp: getElById("health-enemy"),
+  elProgressbar: getElById("progressbar-enemy"),
+  renderHP,
+  renderHPLife,
+  renderProgressbarHP,
+  changeHP,
 };
 
 btn.addEventListener("click", function () {
@@ -59,13 +63,21 @@ function renderProgressbarHP() {
 }
 
 function changeHP(count) {
-  if (this.damage < count) {
+  this.damage -= count;
+
+  const log = this === enemy ? generateLog(this, character, count) : generateLog(this, enemy, count);
+
+  const logElement = document.createElement('p');
+  logElement.innerText = log;
+
+  const logs = document.querySelector('.logs');
+  logs.insertBefore(logElement, logs.children[0]);
+
+  if (this.damage <= 0) {
     this.damage = 0;
     alert(`Бедный ${this.name} проиграл!`);
     btn.disabled = true;
     btn2.disabled = true;
-  } else {
-    this.damage -= count;
   }
 
   this.renderHP();
@@ -73,6 +85,25 @@ function changeHP(count) {
 
 function random(num) {
   return Math.ceil(Math.random() * num);
+}
+
+function generateLog(firstPerson, secondPerson, damage) {
+  const progressBarDefendPerson = `[${firstPerson.damage}/${firstPerson.defaultHp}]`;
+  const logs = [
+    `${firstPerson.name} вспомнил что-то важное, но неожиданно ${secondPerson.name}, не помня себя от испуга, ударил в предплечье врага. -${damage} HP ${firstPerson.name}:${progressBarDefendPerson}`,
+    `${firstPerson.name} поперхнулся, и за это ${secondPerson.name} с испугу приложил прямой удар коленом в лоб врага. -${damage} HP ${firstPerson.name}:${progressBarDefendPerson}`,
+    `${firstPerson.name} забылся, но в это время наглый ${secondPerson.name}, приняв волевое решение, неслышно подойдя сзади, ударил. -${damage} HP ${firstPerson.name}:${progressBarDefendPerson}`,
+    `${firstPerson.name} пришел в себя, но неожиданно ${secondPerson.name} случайно нанес мощнейший удар. -${damage} HP ${firstPerson.name}:${progressBarDefendPerson}`,
+    // eslint-disable-next-line no-useless-escape
+    `${firstPerson.name} поперхнулся, но в это время ${secondPerson.name} нехотя раздробил кулаком \<вырезанно цензурой\> противника. -${damage} HP ${firstPerson.name}:${progressBarDefendPerson}`,
+    `${firstPerson.name} удивился, а ${secondPerson.name} пошатнувшись влепил подлый удар. -${damage} HP ${firstPerson.name}:${progressBarDefendPerson}`,
+    `${firstPerson.name} высморкался, но неожиданно ${secondPerson.name} провел дробящий удар. -${damage} HP ${firstPerson.name}:${progressBarDefendPerson}`,
+    `${firstPerson.name} пошатнулся, и внезапно наглый ${secondPerson.name} беспричинно ударил в ногу противника -${damage} HP ${firstPerson.name}:${progressBarDefendPerson}`,
+    `${firstPerson.name} расстроился, как вдруг, неожиданно ${secondPerson.name} случайно влепил стопой в живот соперника. -${damage} HP ${firstPerson.name}:${progressBarDefendPerson}`,
+    `${firstPerson.name} пытался что-то сказать, но вдруг, неожиданно ${secondPerson.name} со скуки, разбил бровь сопернику. -${damage} HP ${firstPerson.name}:${progressBarDefendPerson}`
+  ];
+
+  return logs[random(logs.length) - 1];
 }
 
 init();
